4ce06427eb98072d6e637fe99756e45d1c30830b
Fiemues <annesofiepoerksen@gmail.com>
2021-12-02 10:50:04 +0100

    ændret i design

--- Semesterprojektet/EMPanel.Designer.cs
+++ Semesterprojektet/EMPanel.Designer.cs
@@ -29,49 +29,61 @@ namespace Semesterprojektet
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.BoligBtn = new System.Windows.Forms.Button();
+            this.SaeglerBtn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // BoligBtn
             // 
-            this.button1.Location = new System.Drawing.Point(287, 214);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(84, 39);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Bolig";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.BoligBtn.Location = new System.Drawing.Point(144, 155);
+            this.BoligBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.BoligBtn.Name = "BoligBtn";
+            this.BoligBtn.Size = new System.Drawing.Size(133, 87);
+            this.BoligBtn.TabIndex = 0;
+            this.BoligBtn.Text = "Bolig";
+            this.BoligBtn.UseVisualStyleBackColor = true;
+            this.BoligBtn.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // SaeglerBtn
             // 
-            this.button2.Location = new System.Drawing.Point(532, 212);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(84, 40);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Sælger";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.SaeglerBtn.Location = new System.Drawing.Point(415, 155);
+            this.SaeglerBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.SaeglerBtn.Name = "SaeglerBtn";
+            this.SaeglerBtn.Size = new System.Drawing.Size(133, 87);
+            this.SaeglerBtn.TabIndex = 1;
+            this.SaeglerBtn.Text = "Sælger";
+            this.SaeglerBtn.UseVisualStyleBackColor = true;
+            this.SaeglerBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.SaeglerBtn);
+            this.panel1.Controls.Add(this.BoligBtn);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(697, 454);
+            this.panel1.TabIndex = 2;
             // 
             // EMPanel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(900, 562);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(697, 454);
+            this.Controls.Add(this.panel1);
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "EMPanel";
-            this.Text = "Opret";
+            this.Text = "EMPanel";
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button SaeglerBtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button BoligBtn;
     }
 }
