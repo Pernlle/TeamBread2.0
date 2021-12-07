e4f2caa5a5a099df4250d10cb71d4ba447dd3ae5
Fiemues <annesofiepoerksen@gmail.com>
2021-12-07 09:54:07 +0100

    forside ser stadig dårlig ud

--- Semesterprojektet/V2Forside.Designer.cs
+++ Semesterprojektet/V2Forside.Designer.cs
@@ -29,10 +29,17 @@ namespace Semesterprojektet
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // V2Forside
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 560);
+            this.Name = "V2Forside";
             this.Text = "V2Forside";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
