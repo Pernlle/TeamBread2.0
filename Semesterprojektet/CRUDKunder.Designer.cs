794728387f986b9e31a4196999716d209ca75cbf
Fiemues <annesofiepoerksen@gmail.com>
2021-12-10 12:37:02 +0100

    new thins

--- Semesterprojektet/CRUDKunder.Designer.cs
+++ Semesterprojektet/CRUDKunder.Designer.cs
@@ -29,25 +29,245 @@ namespace Semesterprojektet
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tHEDATASETOFALL = new Semesterprojektet.tHEDATASETOFALL();
+            this.kunderBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.kunderTableAdapter = new Semesterprojektet.tHEDATASETOFALLTableAdapters.KunderTableAdapter();
+            this.kIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fNavnDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.eNavnDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.emailDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.saelgerDataGridViewCheckBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.koeberDataGridViewCheckBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.opretBtn = new System.Windows.Forms.Button();
+            this.opdaterBtn = new System.Windows.Forms.Button();
+            this.sletBtn = new System.Windows.Forms.Button();
+            this.fNavnBox = new System.Windows.Forms.TextBox();
+            this.eNavnBox = new System.Windows.Forms.TextBox();
+            this.emailBox = new System.Windows.Forms.TextBox();
+            this.koeberCheck = new System.Windows.Forms.CheckBox();
+            this.saeglerCheck = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tHEDATASETOFALL)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kunderBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.saeglerCheck);
+            this.panel1.Controls.Add(this.koeberCheck);
+            this.panel1.Controls.Add(this.emailBox);
+            this.panel1.Controls.Add(this.eNavnBox);
+            this.panel1.Controls.Add(this.fNavnBox);
+            this.panel1.Controls.Add(this.sletBtn);
+            this.panel1.Controls.Add(this.opdaterBtn);
+            this.panel1.Controls.Add(this.opretBtn);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.Size = new System.Drawing.Size(800, 152);
             this.panel1.TabIndex = 0;
             // 
             // panel2
             // 
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 152);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(800, 140);
+            this.panel2.Size = new System.Drawing.Size(800, 298);
             this.panel2.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.kIDDataGridViewTextBoxColumn,
+            this.fNavnDataGridViewTextBoxColumn,
+            this.eNavnDataGridViewTextBoxColumn,
+            this.emailDataGridViewTextBoxColumn,
+            this.saelgerDataGridViewCheckBoxColumn,
+            this.koeberDataGridViewCheckBoxColumn});
+            this.dataGridView1.DataSource = this.kunderBindingSource;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 298);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tHEDATASETOFALL
+            // 
+            this.tHEDATASETOFALL.DataSetName = "tHEDATASETOFALL";
+            this.tHEDATASETOFALL.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // kunderBindingSource
+            // 
+            this.kunderBindingSource.DataMember = "Kunder";
+            this.kunderBindingSource.DataSource = this.tHEDATASETOFALL;
+            // 
+            // kunderTableAdapter
+            // 
+            this.kunderTableAdapter.ClearBeforeFill = true;
+            // 
+            // kIDDataGridViewTextBoxColumn
+            // 
+            this.kIDDataGridViewTextBoxColumn.DataPropertyName = "kID";
+            this.kIDDataGridViewTextBoxColumn.HeaderText = "kID";
+            this.kIDDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.kIDDataGridViewTextBoxColumn.Name = "kIDDataGridViewTextBoxColumn";
+            this.kIDDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // fNavnDataGridViewTextBoxColumn
+            // 
+            this.fNavnDataGridViewTextBoxColumn.DataPropertyName = "fNavn";
+            this.fNavnDataGridViewTextBoxColumn.HeaderText = "fNavn";
+            this.fNavnDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.fNavnDataGridViewTextBoxColumn.Name = "fNavnDataGridViewTextBoxColumn";
+            // 
+            // eNavnDataGridViewTextBoxColumn
+            // 
+            this.eNavnDataGridViewTextBoxColumn.DataPropertyName = "eNavn";
+            this.eNavnDataGridViewTextBoxColumn.HeaderText = "eNavn";
+            this.eNavnDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.eNavnDataGridViewTextBoxColumn.Name = "eNavnDataGridViewTextBoxColumn";
+            // 
+            // emailDataGridViewTextBoxColumn
+            // 
+            this.emailDataGridViewTextBoxColumn.DataPropertyName = "email";
+            this.emailDataGridViewTextBoxColumn.HeaderText = "email";
+            this.emailDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.emailDataGridViewTextBoxColumn.Name = "emailDataGridViewTextBoxColumn";
+            // 
+            // saelgerDataGridViewCheckBoxColumn
+            // 
+            this.saelgerDataGridViewCheckBoxColumn.DataPropertyName = "saelger";
+            this.saelgerDataGridViewCheckBoxColumn.HeaderText = "saelger";
+            this.saelgerDataGridViewCheckBoxColumn.MinimumWidth = 8;
+            this.saelgerDataGridViewCheckBoxColumn.Name = "saelgerDataGridViewCheckBoxColumn";
+            // 
+            // koeberDataGridViewCheckBoxColumn
+            // 
+            this.koeberDataGridViewCheckBoxColumn.DataPropertyName = "koeber";
+            this.koeberDataGridViewCheckBoxColumn.HeaderText = "koeber";
+            this.koeberDataGridViewCheckBoxColumn.MinimumWidth = 8;
+            this.koeberDataGridViewCheckBoxColumn.Name = "koeberDataGridViewCheckBoxColumn";
+            // 
+            // opretBtn
+            // 
+            this.opretBtn.Location = new System.Drawing.Point(669, 25);
+            this.opretBtn.Name = "opretBtn";
+            this.opretBtn.Size = new System.Drawing.Size(101, 27);
+            this.opretBtn.TabIndex = 0;
+            this.opretBtn.Text = "Opret";
+            this.opretBtn.UseVisualStyleBackColor = true;
+            this.opretBtn.Click += new System.EventHandler(this.opretBtn_Click);
+            // 
+            // opdaterBtn
+            // 
+            this.opdaterBtn.Location = new System.Drawing.Point(669, 58);
+            this.opdaterBtn.Name = "opdaterBtn";
+            this.opdaterBtn.Size = new System.Drawing.Size(101, 27);
+            this.opdaterBtn.TabIndex = 1;
+            this.opdaterBtn.Text = "Opdater";
+            this.opdaterBtn.UseVisualStyleBackColor = true;
+            this.opdaterBtn.Click += new System.EventHandler(this.opdaterBtn_Click);
+            // 
+            // sletBtn
+            // 
+            this.sletBtn.Location = new System.Drawing.Point(669, 92);
+            this.sletBtn.Name = "sletBtn";
+            this.sletBtn.Size = new System.Drawing.Size(101, 27);
+            this.sletBtn.TabIndex = 2;
+            this.sletBtn.Text = "Slet";
+            this.sletBtn.UseVisualStyleBackColor = true;
+            this.sletBtn.Click += new System.EventHandler(this.sletBtn_Click);
+            // 
+            // fNavnBox
+            // 
+            this.fNavnBox.Location = new System.Drawing.Point(103, 39);
+            this.fNavnBox.Name = "fNavnBox";
+            this.fNavnBox.Size = new System.Drawing.Size(100, 26);
+            this.fNavnBox.TabIndex = 3;
+            this.fNavnBox.TextChanged += new System.EventHandler(this.fNavnBox_TextChanged);
+            // 
+            // eNavnBox
+            // 
+            this.eNavnBox.Location = new System.Drawing.Point(103, 96);
+            this.eNavnBox.Name = "eNavnBox";
+            this.eNavnBox.Size = new System.Drawing.Size(100, 26);
+            this.eNavnBox.TabIndex = 4;
+            this.eNavnBox.TextChanged += new System.EventHandler(this.eNavnBox_TextChanged);
+            // 
+            // emailBox
+            // 
+            this.emailBox.Location = new System.Drawing.Point(304, 39);
+            this.emailBox.Name = "emailBox";
+            this.emailBox.Size = new System.Drawing.Size(100, 26);
+            this.emailBox.TabIndex = 5;
+            this.emailBox.TextChanged += new System.EventHandler(this.emailBox_TextChanged);
+            // 
+            // koeberCheck
+            // 
+            this.koeberCheck.AutoSize = true;
+            this.koeberCheck.Location = new System.Drawing.Point(327, 98);
+            this.koeberCheck.Name = "koeberCheck";
+            this.koeberCheck.Size = new System.Drawing.Size(77, 24);
+            this.koeberCheck.TabIndex = 7;
+            this.koeberCheck.Text = "Køber";
+            this.koeberCheck.UseVisualStyleBackColor = true;
+            this.koeberCheck.CheckedChanged += new System.EventHandler(this.koeberCheck_CheckedChanged);
+            // 
+            // saeglerCheck
+            // 
+            this.saeglerCheck.AutoSize = true;
+            this.saeglerCheck.Location = new System.Drawing.Point(234, 98);
+            this.saeglerCheck.Name = "saeglerCheck";
+            this.saeglerCheck.Size = new System.Drawing.Size(87, 24);
+            this.saeglerCheck.TabIndex = 8;
+            this.saeglerCheck.Text = "Sælger";
+            this.saeglerCheck.UseVisualStyleBackColor = true;
+            this.saeglerCheck.CheckedChanged += new System.EventHandler(this.saeglerCheck_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Fornavn";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Efternavn";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(230, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Email";
             // 
             // CRUDKunder
             // 
@@ -58,6 +278,13 @@ namespace Semesterprojektet
             this.Controls.Add(this.panel1);
             this.Name = "CRUDKunder";
             this.Text = "CRUDKunder";
+            this.Load += new System.EventHandler(this.CRUDKunder_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tHEDATASETOFALL)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kunderBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -66,5 +293,26 @@ namespace Semesterprojektet
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private tHEDATASETOFALL tHEDATASETOFALL;
+        private System.Windows.Forms.BindingSource kunderBindingSource;
+        private tHEDATASETOFALLTableAdapters.KunderTableAdapter kunderTableAdapter;
+        private System.Windows.Forms.Button sletBtn;
+        private System.Windows.Forms.Button opdaterBtn;
+        private System.Windows.Forms.Button opretBtn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn kIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fNavnDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn eNavnDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn emailDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn saelgerDataGridViewCheckBoxColumn;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn koeberDataGridViewCheckBoxColumn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox saeglerCheck;
+        private System.Windows.Forms.CheckBox koeberCheck;
+        private System.Windows.Forms.TextBox emailBox;
+        private System.Windows.Forms.TextBox eNavnBox;
+        private System.Windows.Forms.TextBox fNavnBox;
     }
 }
--- Semesterprojektet/CRUDKunder.cs
+++ Semesterprojektet/CRUDKunder.cs
@@ -7,14 +7,160 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Semesterprojektet
 {
+    
     public partial class CRUDKunder : Form
     {
+        public string strconn = @"Server=den1.mssql7.gear.host; Database=teambreaddb; User ID=teambreaddb; Password=Tg53?N_p6fzh";
         public CRUDKunder()
         {
             InitializeComponent();
+        }
+
+        private void CRUDKunder_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'tHEDATASETOFALL.Kunder' table. You can move, or remove it, as needed.
+            this.kunderTableAdapter.Fill(this.tHEDATASETOFALL.Kunder);
+
+        }
+
+        private void koeberCheck_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saeglerCheck_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void emailBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fNavnBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void eNavnBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void opretBtn_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(strconn);
+
+            if (saeglerCheck.Checked && koeberCheck.Checked)
+            {
+                if (fNavnBox.Text != "" && eNavnBox.Text != "" && emailBox.Text != "")
+                {
+                    // Definerer strings, for at kunne converte værdien til den korrekte værdi i databasen når man inserter (create)
+                    string badresse = adresse.Text;
+                    string bpostnr = postnr.Text;
+                    string bkvm = kvm.Text;
+                    string spris = salgspris.Text;
+                    string saegler = sID.Text;
+                    string ejendomsmaegler = eID.Text;
+
+                    string hpris = handelspris.Text;
+                    // Handelsdatoen er en value, fordi vi bruger DateTimePicker, for at gemme informationen i databasen converter vi den til en string
+                    string hdato = Convert.ToString(handelsdato.Value);
+
+                    string sqlCom = "INSERT INTO Bolig(adresse,postNr,kvm,salgsPris,handelsPris,handelsDato,kID,eID,solgt) VALUES (@adresse, @postNr, @kvm, @salgsPris, @handelsPris, @handelsDato, @kID, @eID,'1');";
+                    SqlCommand cmd = new SqlCommand(sqlCom, conn);
+
+                    cmd.Parameters.Add("@adresse", System.Data.SqlDbType.VarChar);
+                    cmd.Parameters["@adresse"].Value = Convert.ToString(badresse);
+                    cmd.Parameters.Add("@postNr", System.Data.SqlDbType.Int);
+                    cmd.Parameters["@postNr"].Value = Convert.ToInt32(bpostnr);
+                    cmd.Parameters.Add("@kvm", System.Data.SqlDbType.Decimal);
+                    cmd.Parameters["@kvm"].Value = Convert.ToDecimal(bkvm);
+                    cmd.Parameters.Add("@salgsPris", System.Data.SqlDbType.Decimal);
+                    cmd.Parameters["@salgsPris"].Value = Convert.ToDecimal(spris);
+                    cmd.Parameters.Add("@kID", System.Data.SqlDbType.Int);
+                    cmd.Parameters["@kID"].Value = Convert.ToInt32(saegler);
+                    cmd.Parameters.Add("@eID", System.Data.SqlDbType.Int);
+                    cmd.Parameters["@eID"].Value = Convert.ToInt32(ejendomsmaegler);
+
+                    cmd.Parameters.Add("@handelsPris", System.Data.SqlDbType.Decimal);
+                    cmd.Parameters["@handelsPris"].Value = Convert.ToDecimal(hpris);
+                    cmd.Parameters.Add("@handelsDato", System.Data.SqlDbType.VarChar);
+                    cmd.Parameters["@handelsDato"].Value = Convert.ToString(hdato);
+
+                    try
+                    {
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                        //conn.Close(); // remember this HUSK ALTID AT LUKKE!
+                        MessageBox.Show("Solgt bolig oprettet");
+                        this.boligTableAdapter.Fill(this.tHEONETHEONLY.Bolig);
+                    }
+                    catch (Exception exc)
+                    {
+                        MessageBox.Show("ERROR: \n\n" + exc.ToString());
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Tast alle celler ind");
+                }
+            }
+
+            // HVIS DEN IKKE ER SOLGT
+            else
+            {
+                string badresse = adresse.Text;
+                string bpostnr = postnr.Text;
+                string bkvm = kvm.Text;
+                string spris = salgspris.Text;
+                string saegler = sID.Text;
+                string ejendomsmaegler = eID.Text;
+
+                string sqlCom = "INSERT INTO Bolig(adresse,postNr,kvm,salgsPris,kID,eID,solgt) VALUES (@adresse, @postNr, @kvm, @salgsPris, @kID, @eID,'0');";
+                SqlCommand cmd = new SqlCommand(sqlCom, conn);
+
+                cmd.Parameters.Add("@adresse", System.Data.SqlDbType.VarChar);
+                cmd.Parameters["@adresse"].Value = Convert.ToString(badresse);
+                cmd.Parameters.Add("@postNr", System.Data.SqlDbType.Int);
+                cmd.Parameters["@postNr"].Value = Convert.ToInt32(bpostnr);
+                cmd.Parameters.Add("@kvm", System.Data.SqlDbType.Decimal);
+                cmd.Parameters["@kvm"].Value = Convert.ToDecimal(bkvm);
+                cmd.Parameters.Add("@salgsPris", System.Data.SqlDbType.Decimal);
+                cmd.Parameters["@salgsPris"].Value = Convert.ToDecimal(spris);
+                cmd.Parameters.Add("@kID", System.Data.SqlDbType.Int);
+                cmd.Parameters["@kID"].Value = Convert.ToInt32(saegler);
+                cmd.Parameters.Add("@eID", System.Data.SqlDbType.Int);
+                cmd.Parameters["@eID"].Value = Convert.ToInt32(ejendomsmaegler);
+
+                try
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    //conn.Close(); // remember this HUSK ALTID AT LUKKE!
+                    MessageBox.Show("Ny bolig oprettet");
+                    this.boligTableAdapter.Fill(this.tHEONETHEONLY.Bolig);
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show("ERROR: \n\n" + exc.ToString());
+                }
+            }
+        }
+
+        private void opdaterBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sletBtn_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
